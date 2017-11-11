8b2edef352184b765cb15644e229dcfc2795fc62
MininMobile <miningamer@gmail.com>
2017-11-11 14:22:37 +0000

    More Things

--- actions.js
+++ actions.js
@@ -1,29 +1,38 @@
 var v;
 var s;
 
-$(document).ready(function() {
+function onLoad() {
     v = document.getElementById("player");
     s = document.getElementsByClassName("slideBtn")[0];
 
     // Init Slider
-    s.min = 0;
-    s.max = (v.duration).toString();
+    s.setAttribute("min", "0");
+    s.setAttribute("max", v.duration);
     updateSlider();
     setInterval(function(){updateSlider()}, 500);
 
     // Init Play
-    $(".play").click(function() { playPause(); })
-})
+    $(".play").click(function() { playPause(v.paused); })
+}
 $(document).on('input', s, function() { updateVideo(); });
 
-function updateSlider() { s.value = v.currentTime; }
+function updateSlider() {
+    s.value = v.currentTime;
+    if (s.value == Math.floor(v.duration)) {
+        playPause(false);
+    }
+}
 function updateVideo() { v.currentTime = s.value; }
-function playPause() {
-    if (v.paused) {
+
+function playPause(paused) {
+    if (paused) {
+        if (s.value == Math.floor(v.duration)) {
+            v.currentTime = 0;
+        }
         v.play();
-        $(".play").html(' <div class="pbtn pauseBtn"> ');
+        $(".play").html(' <div class="cbtn pauseBtn"> ');
     } else {
         v.pause();
-        $(".play").html(' <div class="pbtn playBtn"> ');
+        $(".play").html(' <div class="cbtn playBtn"> ');
     }
 }
